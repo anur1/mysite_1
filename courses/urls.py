bef6d1c78bb113a80d8e0ad0c85c9a861176a27a
A Nuri <ahmettonuri@gmail.com>
2024-06-15 20:57:31 +0300

    deneme

--- courses/urls.py
+++ courses/urls.py
@@ -9,8 +9,4 @@ urlpatterns = [
     path('<kurs_adi>', views.details),
     path('kategori/<int:category_id>', views.getCoursesByCategoryId),
     path('kategori/<str:category_name>', views.getCoursesByCategory, name='courses_by_category'),
-
-
-
-
 ]
