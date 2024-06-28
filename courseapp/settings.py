9976c38c2aa3d213d18a91451ad2ad38c9a0f3e6
A Nuri <ahmettonuri@gmail.com>
2024-06-28 15:29:33 +0300

    static?url revised

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -119,10 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'   ##28 Haziran'da en başına / eklendi
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    "courses/static/courses/",  ## 28 Haziran da eklendi, silinebilir
+
 ]
 
 # Default primary key field type
